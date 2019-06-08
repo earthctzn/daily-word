aed3a63a03b310366e5785c3db0c79939bb7342e
earthctzn <earthctzn1@gmail.com>
2019-06-08 04:33:10 -0400

    Getting closer

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,14 +7,18 @@ class DailyWord::Scraper
         words = bulk.css("div.more-words-of-day-container h3")
         list = words.each_with_index{|month, i| puts "#{i+1}. #{month.text}"}
         list
+        binding.pry
     end
 
     def self.get_words
         list = {}
         bulk = Nokogiri::HTML(open(DATA))
         dates = bulk.css("ul.more-wod-items h4").to_a
-        dates.each{|date| list{'#{date}:'}}
-        list
+        dates.each{|date| list{"#{date}:"}}
+        # possibly need to split this into 2 methods or I may have to create my hash here with date: word: symbols.
+        words = bulk.css("h2 a")
+        words.each do |url| 
+            list{url.attr("href")}
     end
 
 end
