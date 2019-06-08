5ec581e7cdf092ca76a852b5692b436cd7c82837
earthctzn <earthctzn1@gmail.com>
2019-06-07 21:52:34 -0400

    Scraper is scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,13 @@
 class DailyWord::Scraper
 
+    DATA = "https://www.merriam-webster.com/word-of-the-day/calendar"
+
     def self.get_words 
+        bulk = Nokogiri::HTML(open(DATA))
+        words = bulk.css("div.more-words-of-day-container h3")
+        # words.each do |month|
+
+        binding.pry 
        puts "raw data file"
     end
 end
