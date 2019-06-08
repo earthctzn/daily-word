1b3c53a7e1968af6298436bb39b546dd43c80ae1
earthctzn <earthctzn1@gmail.com>
2019-06-08 03:16:22 -0400

    Scraper get_words method scraping correct data

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,8 +3,17 @@ class DailyWord::CLI
 
 
     def self.call
-        puts "It's working!" 
-         DailyWord::Scraper.get_words
+        
+        puts "Hi there! Welcome to DailyWord!" 
+        puts "Please choose a month to see the daily words."
+        puts "To select a month, please type the corresponding number"
+         DailyWord::Scraper.get_calendar
+         user_input = gets.chomp -1 
+         
+        #  if user_input = "1"
+        #     DailyWord::Scraper.get_words
+
+         
     end 
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,12 +2,19 @@ class DailyWord::Scraper
 
     DATA = "https://www.merriam-webster.com/word-of-the-day/calendar"
 
-    def self.get_words 
+    def self.get_calendar 
         bulk = Nokogiri::HTML(open(DATA))
         words = bulk.css("div.more-words-of-day-container h3")
-        words.each_with_index{|month, i| puts "#{i+1}. #{month.text}"}
-
-        binding.pry 
-       puts "raw data file"
+        list = words.each_with_index{|month, i| puts "#{i+1}. #{month.text}"}
+        list
     end
+
+    def self.get_words
+        list = {}
+        bulk = Nokogiri::HTML(open(DATA))
+        dates = bulk.css("ul.more-wod-items h4").to_a
+        dates.each{|date| list{'#{date}:'}}
+        list
+    end
+
 end
