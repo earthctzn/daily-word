b6a3c15619079c09274022685a042e6dda373867
earthctzn <earthctzn1@gmail.com>
2019-06-07 21:16:44 -0400

    Initialize method added

--- lib/word.rb
+++ lib/word.rb
@@ -2,6 +2,13 @@ class DailyWord::Word
     attr_accessor :date, :definition
     @@all = []
 
+    def initialize
+        @date = date
+        @definition = definition
+        @@all << self 
+    end
+
+    
     def self.all
         @@all
     end 
