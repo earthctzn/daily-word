306ff7eea30bc225e9faecf3f65e5ab68d6ea3bd
earthctzn <earthctzn1@gmail.com>
2019-06-10 19:48:16 -0400

    Updated notes and continuing to stub out CLI class.

--- NOTES.md
+++ NOTES.md
@@ -1,10 +1,18 @@
 # The Idea
 
-I will build an gem that will provide the user with a Word of the Day along with the definition, example use cases, and pronunciation. The gem will have a CLI that will allow the user to view past Words of the Day.
+I will build a gem that will provide the user with a Word of the Day along with the definition, example use cases, and pronunciation. The gem will have a CLI that will allow the user to view past Words of the Day.
 
 ## The Plan
+1. Plan the Gem. Imagine the CLI
+2. Start with the project structure. I used Bundler.
+3. Start with the entry point - the file run
+4. Force that.
 
+### Behavior
 I'd like the app to start with: 
 1. `greeting` the user.
-2. Displaying the word of day `wod` with full attributes i.e date, definition, example use cases, and pronunciation. (Pronunciation should be clickable to listen to the audio clip.)
-3. Ask the user "Would you like to see more Words of the Day?
+2. Display the word of day `wod` with full attributes i.e date, definition, example use cases, and pronunciation. (Pronunciation should be clickable to listen to the audio clip.)
+3. Ask the user for input.
+
+
+26:52
--- lib/cli.rb
+++ lib/cli.rb
@@ -3,28 +3,27 @@ class DailyWord::CLI
 
 
     def self.call
-        user_input = "1..13"
-        until user_input == "exit"
-            puts "Hi there! Welcome to DailyWord!" 
-            # Dailyword::Scraper.wod 
-            puts "Please choose a month to see the daily words."
-            puts "To select a month, please type the corresponding number"
-             DailyWord::Scraper.get_words
-            # DailyWord::Scraper.get_calendar
-            user_input = gets.chomp
-         
-         case 
-            when user_input == "1"
-                puts "Okayyy!"
-                #DailyWord::Word
-            when user_input == "exit"
-                puts "Good bye!"
-            else
-                puts "I dont understand that."
-            end
-        end
+        puts "Hi there! Welcome to DailyWord!" 
+        # Dailyword::Scraper.wod 
+        puts "Please choose a month to see the daily words."
+        puts "To select a month, please type the corresponding number"
+         DailyWord::Scraper.get_words
+        # DailyWord::Scraper.get_calendar
+        user_input = nil
+        user_input = gets.chomp
 
          
     end 
-
+     
+    def instructions
+        case input
+        when user_input == "1"
+            puts "Okayyy!"
+            #DailyWord::Word
+        when user_input == "exit"
+            puts "Good bye!"
+        else
+            puts "I dont understand that."
+        end
+    end
 end
